509a5e0f4f73da202b8476a64e46655d12290e12
AhmetSelman6118 <ahmetselman1083@gmail.com>
2026-05-02 17:04:09 +0300

    Channel

--- src/api_handler.py
+++ src/api_handler.py
@@ -51,28 +51,62 @@ def get_video_details(video_id):
 
 def get_bulk_video_details(video_ids):
     """
-    ID listesi verilen videoların detaylarını toplu halde çeker.
+    Video ID'lerini alır; hem video istatistiklerini hem de 
+    ilgili kanalın bilgilerini çekip birleştirir.
     """
     youtube = get_youtube_client()
     
-    request = youtube.videos().list(
+    # 1. Adım: Videoların temel detaylarını ve istatistiklerini çek
+    video_request = youtube.videos().list(
         part="snippet,statistics,contentDetails",
         id=",".join(video_ids)
     )
-    response = request.execute()
+    video_response = video_request.execute()
     
+    # 2. Adım: Bu videolara ait benzersiz Kanal ID'lerini listele
+    # (Set kullanarak aynı kanalın bilgilerini tekrar tekrar istemiyoruz - Kota dostu!)
+    channel_ids = list(set([item['snippet']['channelId'] for item in video_response.get('items', [])]))
+    
+    # 3. Adım: Kanalların abone ve toplam izlenme sayılarını çek (Toplu sorgu)
+    channel_request = youtube.channels().list(
+        part="statistics",
+        id=",".join(channel_ids)
+    )
+    channel_response = channel_request.execute()
+    
+    # Kanal bilgilerini hızlıca bulabilmek için bir sözlük (map) oluşturalım
+    channels_map = {
+        item['id']: {
+            "channel_subscribers": int(item['statistics'].get('subscriberCount', 0)),
+            "channel_total_views": int(item['statistics'].get('viewCount', 0)),
+            "channel_total_videos": int(item['statistics'].get('videoCount', 0))
+        } for item in channel_response.get('items', [])
+    }
+    
+    # 4. Adım: Video verileriyle kanal verilerini harmanla
     all_video_data = []
-    for item in response.get('items', []):
+    for item in video_response.get('items', []):
+        c_id = item['snippet']['channelId']
+        # Kanala dair verileri al, bulamazsan varsayılan 0 ata
+        c_stats = channels_map.get(c_id, {
+            "channel_subscribers": 0, 
+            "channel_total_views": 0, 
+            "channel_total_videos": 0
+        })
+        
         data = {
             "video_id": item['id'],
             "title": item['snippet']['title'],
             "published_at": item['snippet']['publishedAt'],
             "category_id": item['snippet']['categoryId'],
             "duration": item['contentDetails']['duration'],
-            # BURASI KRİTİK: API'den gelen isimler viewCount, likeCount, commentCount şeklindedir.
             "view_count": int(item['statistics'].get('viewCount', 0)),
             "like_count": int(item['statistics'].get('likeCount', 0)),
-            "comment_count": int(item['statistics'].get('commentCount', 0))
+            "comment_count": int(item['statistics'].get('commentCount', 0)),
+            # Kanal bilgileri yeni sütunlar olarak ekleniyor
+            "channel_subscribers": c_stats["channel_subscribers"],
+            "channel_total_views": c_stats["channel_total_views"],
+            "channel_total_videos": c_stats["channel_total_videos"]
         }
         all_video_data.append(data)
     
